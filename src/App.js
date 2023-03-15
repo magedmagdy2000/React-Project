478b9b7c53589e7dcda7501f949e6fd667c7f9f3
magedmagdy2000 <100667701+magedmagdy2000@users.noreply.github.com>
2023-03-15 13:04:06 +0200

    edit app.js file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>maged magdy</h1>
+      <h2>maged magdy</h2>
+      
+    </>
+    );
 }
 
 export default App;
